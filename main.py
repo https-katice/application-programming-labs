a26dd4e723f94c524946442ff76b04e7c1d65913
https-katice <164325850+https-katice@users.noreply.github.com>
2024-12-19 13:50:05 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import cv2
-
-import sys
 import argparse
+
+import cv2
 import matplotlib.pyplot as plt
+
 from image import read_image, convert_to_grayscale, plot_histogram, save_image
 
 
@@ -11,33 +11,10 @@ def get_args() -> argparse.Namespace:
     Читает аргументы из терминала
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument("file_name", type = str, help="Keyword of search request")
-    parser.add_argument("-d", "--imgdir", type = str, help="Path to the folder, where you want to save images")
-    parser.add_argument("-f", "--flag", type = bool, help= "Grayscale true or false")
+    parser.add_argument("image_path", type = str, help="")
+    parser.add_argument("save_path", type = str, help="")
     arguments = parser.parse_args()
-    print(arguments.flag)
     return arguments
-
-
-def main(image_path: str, save_path: str) -> None:
-    try:
-
-        original_image = read_image(image_path)
-
-        print(f"Размер изображения: {original_image.shape[1]}x{original_image.shape[0]}")
-
-        grayscale_image = convert_to_grayscale(original_image)
-
-        plot_histogram(original_image, "оригинального изображения")
-
-        plot_histogram(grayscale_image, "полутонового изображения")
-
-        display_images(original_image, grayscale_image)
-
-        save_image(grayscale_image, save_path)
-
-    except Exception as e:
-        print(f"Произошла ошибка: {e}")
 
 
 def display_images(original_image, grayscale_image) -> None:
@@ -61,11 +38,30 @@ def display_images(original_image, grayscale_image) -> None:
     plt.show()
 
 
-if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print("Использование: python main.py <путь_к_изображению> <путь_для_сохранения>")
-        sys.exit(1)
+def main() -> None:
+    try:
+        args = get_args()
 
-    image_path = sys.argv[1]
-    save_path = sys.argv[2]
-    main(image_path, save_path)
+        image_path = args.image_path
+        save_path = args.save_path
+
+        original_image = read_image(image_path)
+
+        print(f"Размер изображения: {original_image.shape[1]}x{original_image.shape[0]}")
+
+        grayscale_image = convert_to_grayscale(original_image)
+
+        plot_histogram(original_image, "оригинального изображения")
+
+        plot_histogram(grayscale_image, "полутонового изображения")
+
+        display_images(original_image, grayscale_image)
+
+        save_image(grayscale_image, save_path)
+
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
+
+
+if __name__ == '__main__':
+    main()
