6b9c35f573457c776edec196903dd2107bd0d43b
https-katice <164325850+https-katice@users.noreply.github.com>
2024-12-19 12:18:04 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,67 +1,71 @@
+import cv2
+
+import sys
 import argparse
-import re
+import matplotlib.pyplot as plt
+from image import read_image, convert_to_grayscale, plot_histogram, save_image
 
 
-def open_file(filename: str) -> str:
+def get_args() -> argparse.Namespace:
     """
-    The Function opens and reads a file
-    :param filename: name file
-    :return: file contents as a string
-    """
-    with open(filename, "r", encoding="UTF-8") as file:
-        return file.read()
-
-
-def split(filename: str) -> list[str]:
-    """
-    The Function splits a string into substrings
-    :return: list of questionnaires separately
-    """
-    pattern = r'\d+' + r'[)]' + '\n'
-    return re.split(pattern, filename, maxsplit=0)
-
-
-def parsing() -> str:
-    """
-    Parse command line arguments and returns the file name
-    :return: file name
+    Читает аргументы из терминала
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument('filename', type=str, help='The name of the file to analyze')
-    args = parser.parse_args()
-    return args.filename
+    parser.add_argument("file_name", type = str, help="Keyword of search request")
+    parser.add_argument("-d", "--imgdir", type = str, help="Path to the folder, where you want to save images")
+    parser.add_argument("-f", "--flag", type = bool, help= "Grayscale true or false")
+    arguments = parser.parse_args()
+    print(arguments.flag)
+    return arguments
 
 
-def search(divided: list[str]) -> list[str]:
-    """
-    Function search for questionnaires of people who lives in Moscow
-    :return: all questionnaires which fits in
-    """
-    found = [i for i in divided if re.search(r'Москва', i)]
-    return found
-
-
-def out(found: list[str]) -> None:
-    """
-    Displaying of required questionnaires
-    :param found: list of required questionnaires
-    :return: nothing
-    """
-    print("\n")
-    for j in found:
-        print(j)
-
-
-def main():
+def main(image_path: str, save_path: str) -> None:
     try:
-        filename = parsing()
-        doc = open_file(filename)
-        divided = split(doc)
-        found = search(divided)
-        out(found)
-    except Exception as exc:
-        print(f"Error {exc}")
+
+        original_image = read_image(image_path)
+
+        print(f"Размер изображения: {original_image.shape[1]}x{original_image.shape[0]}")
+
+        grayscale_image = convert_to_grayscale(original_image)
+
+        plot_histogram(original_image, "оригинального изображения")
+
+        plot_histogram(grayscale_image, "полутонового изображения")
+
+        display_images(original_image, grayscale_image)
+
+        save_image(grayscale_image, save_path)
+
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
+
+
+def display_images(original_image, grayscale_image) -> None:
+    """
+    Отображает оригинальное и полутоновое изображение
+    """
+    plt.figure(figsize=(12, 6))
+
+    # Отображение оригинального изображения
+    plt.subplot(1, 2, 1)
+    plt.imshow(cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB))
+    plt.title("Оригинальное изображение")
+    plt.axis('off')
+
+    # Отображение полутонового изображения
+    plt.subplot(1, 2, 2)
+    plt.imshow(grayscale_image, cmap='gray')
+    plt.title("Полутоновое изображение")
+    plt.axis('off')
+    plt.tight_layout()
+    plt.show()
 
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) != 3:
+        print("Использование: python main.py <путь_к_изображению> <путь_для_сохранения>")
+        sys.exit(1)
+
+    image_path = sys.argv[1]
+    save_path = sys.argv[2]
+    main(image_path, save_path)
